32b012e46d2d28729a3c8a79abcc594ead324e8d
Kelvin Gobo <decodehacks@localhost.localdomain>
2018-06-30 00:03:38 +0100

    added width=device-width to viewport meta for mobile responsiveness

--- designs.js
+++ designs.js
@@ -1,30 +1,30 @@
 // Select color input
 // Select size input
 var gridHeight, gridWidth, canv, color;
-$("#sizePicker").on("submit",function(x){
-	x.preventDefault();
-    canv=$('#pixelCanvas');
-    gridHeight=$('#inputHeight'). val();
-    gridWidth=$('#inputWeight'). val();
+$("#sizePicker").on("submit", function(x) {
+  x.preventDefault();
+  canv = $('#pixelCanvas');
+  gridHeight = $('#inputHeight').val();
+  gridWidth = $('#inputWeight').val();
 
-// When size is submitted by the user, call makeGrid()
-makeGrid(gridHeight,gridWidth);
+  // When size is submitted by the user, call makeGrid()
+  makeGrid(gridHeight, gridWidth);
 })
 
 function makeGrid(gridHeight, gridWidth) {
-    $('tr').remove();
-    // Your code goes here!
+  $('tr').remove();
+  // Your code goes here!
 
-    for (var i = 1; i <= gridHeight; i++) {
-        canv.append('<tr id=table'+ i + '></tr>');
-        for (var j = 1; j <=gridWidth; j++) {
-            $('#table'+ i).append('<td></td>');
-        }
+  for (var i = 1; i <= gridHeight; i++) {
+    canv.append('<tr id=table' + i + '></tr>');
+    for (var j = 1; j <= gridWidth; j++) {
+      $('#table' + i).append('<td></td>');
     }
-    //pick a color
-    $('td').on('click',function(x) {
-        color=$('#colorPicker').val();
-		$(this).attr('bgcolor', color);
+  }
+  //pick a color
+  $('td').on('click', function(x) {
+    color = $('#colorPicker').val();
+    $(this).attr('bgcolor', color);
 
-    })
+  })
 }
